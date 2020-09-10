2e03aba39595bb5d46f95958e0f657d798271a47
olehhapuk <oleggapuk20@gmail.com>
2020-09-10 19:00:22 +0300

    Added template for 3 lesson

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import Panel from './Panel';
-import Product from './Product';
 
 export default function App() {
-  return (
-    <Panel title="Best selling products">
-      <Product name="Tacos With Lime" price={10.99} quantity={5} />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-        quantity={0}
-      />
-    </Panel>
-  );
+  return <Panel title="Lesson 3: Events and State"></Panel>;
 }
