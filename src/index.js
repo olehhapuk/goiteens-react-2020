5d843ff78239ad38f3fe0a3b20acaa4db1329c32
olehhapuk <oleggapuk20@gmail.com>
2020-10-18 12:59:11 +0300

    Made Counter and started making SignUpForm

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-import App from './components/App';
+// import App from './components/App';
+// import Counter from './components/Counter';
+import SignUpForm from './components/SignUpForm';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SignUpForm />, document.getElementById('root'));
