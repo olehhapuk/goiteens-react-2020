f5aa343601b55e9f869da2a33062357a69691876
olehhapuk <oleggapuk20@gmail.com>
2020-11-28 16:57:18 +0200

    Init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+import './index.css';
 
-// http://hn.algolia.com/api/v1/search?query=react&page=0&hitsPerPage=5
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<h1>Hello, World!</h1>, document.getElementById('root'));
--- src/services/articlesApi.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function fetchArticlesByQuery(query, page) {
-  return axios
-    .get(
-      `http://hn.algolia.com/api/v1/search?query=${query}&page=${page}&hitsPerPage=5`
-    )
-    .then((response) => response.data.hits);
-}
