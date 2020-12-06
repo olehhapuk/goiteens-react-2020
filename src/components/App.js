38272b18f8b72d970d5ea83abccf8b13869ce68b
olehhapuk <oleggapuk20@gmail.com>
2020-12-06 12:30:57 +0200

    Route, matching, Route props, links

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,40 @@
+import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
+
+import HomeView from '../views/HomeView';
+import AboutView from '../views/AboutView';
+import NotFoundView from '../views/NotFoundView';
+
 export default function App() {
   return (
     <>
-      <p>App</p>
+      <ul>
+        <li>
+          <NavLink
+            exact
+            to="/"
+            className="Nav-link"
+            activeClassName="Nav-link_active"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className="Nav-link"
+            activeClassName="Nav-link_active"
+          >
+            About
+          </NavLink>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route exact path="/" component={HomeView} />
+        <Route path="/about" component={AboutView} />
+        <Route path="/404" component={NotFoundView} />
+        <Redirect to="/404" />
+      </Switch>
     </>
   );
 }
---
+++ src/views/NotFoundView.js
@@ -0,0 +1,7 @@
+export default function NotFoundView() {
+  return (
+    <div>
+      <h2>404. Not found!</h2>
+    </div>
+  );
+}
