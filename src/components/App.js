25c6e42292b6068c21ac0595025dc54a6dc9564a
olehhapuk <oleggapuk20@gmail.com>
2021-02-28 00:14:44 +0200

    Removed routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,51 @@
-import { Switch, Route } from 'react-router-dom';
+import { Component } from 'react';
 
-import { routes } from '../routes';
+import createTask from '../utils/createTask';
 
-function App() {
-  return (
-    <Switch>
-      {routes.map((route) => (
-        <Route key={route.path} {...route} />
-      ))}
-    </Switch>
-  );
+import Layout from './Layout/Layout';
+import TaskEditor from './TaskEditor/TaskEditor';
+import TaskList from './TaskList/TaskList';
+
+export default class App extends Component {
+  state = {
+    tasks: [],
+  };
+
+  handleAddTask = () => {
+    const newTask = createTask();
+
+    this.setState((prevState) => {
+      return {
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  };
+
+  handleRemoveTask = (id) => {
+    this.setState((prevState) => {
+      return {
+        tasks: prevState.tasks.filter((task) => {
+          if (task.id === id) {
+            return false;
+          } else {
+            return true;
+          }
+        }),
+      };
+    });
+  };
+
+  render() {
+    const { tasks } = this.state;
+
+    return (
+      <Layout title="Todo App">
+        <TaskEditor addTask={this.handleAddTask} />
+
+        {tasks.length > 0 && (
+          <TaskList tasks={tasks} removeTask={this.handleRemoveTask} />
+        )}
+      </Layout>
+    );
+  }
 }
-
-export default App;
