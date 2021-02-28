2068b3c4822dcb4f5e669f016e60b6b7022e43fb
olehhapuk <oleggapuk20@gmail.com>
2021-02-28 09:34:06 +0200

    Init

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,11 @@
-import { Component } from 'react';
+import Todo from './Todo/Todo';
 
-import createTask from '../utils/createTask';
-
-import Layout from './Layout/Layout';
-import TaskEditor from './TaskEditor/TaskEditor';
-import TaskList from './TaskList/TaskList';
-
-export default class App extends Component {
-  state = {
-    tasks: [],
-  };
-
-  handleAddTask = () => {
-    const newTask = createTask();
-
-    this.setState((prevState) => {
-      return {
-        tasks: [...prevState.tasks, newTask],
-      };
-    });
-  };
-
-  handleRemoveTask = (id) => {
-    this.setState((prevState) => {
-      return {
-        tasks: prevState.tasks.filter((task) => {
-          if (task.id === id) {
-            return false;
-          } else {
-            return true;
-          }
-        }),
-      };
-    });
-  };
-
-  render() {
-    const { tasks } = this.state;
-
-    return (
-      <Layout title="Todo App">
-        <TaskEditor addTask={this.handleAddTask} />
-
-        {tasks.length > 0 && (
-          <TaskList tasks={tasks} removeTask={this.handleRemoveTask} />
-        )}
-      </Layout>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
 }
+
+export default App;
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,59 @@
+import { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+import createTask from '../../utils/createTask';
+
+import Layout from '../Layout/Layout';
+import TaskEditor from '../TaskEditor/TaskEditor';
+import TaskList from '../TaskList/TaskList';
+
+export default class Todo extends Component {
+  state = {
+    tasks: [],
+  };
+
+  handleAddTask = () => {
+    const newTask = createTask();
+
+    this.setState((prevState) => {
+      return {
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  };
+
+  handleRemoveTask = (id) => {
+    this.setState((prevState) => {
+      return {
+        tasks: prevState.tasks.filter((task) => {
+          if (task.id === id) {
+            return false;
+          } else {
+            return true;
+          }
+        }),
+      };
+    });
+  };
+
+  render() {
+    const { tasks } = this.state;
+
+    return (
+      <Layout title="Todo App">
+        <CSSTransition in appear classNames="fade" timeout={250} unmountOnExit>
+          <TaskEditor addTask={this.handleAddTask} />
+        </CSSTransition>
+
+        <CSSTransition
+          in={tasks.length > 0}
+          classNames="fade"
+          timeout={250}
+          unmountOnExit
+        >
+          <TaskList tasks={tasks} removeTask={this.handleRemoveTask} />
+        </CSSTransition>
+      </Layout>
+    );
+  }
+}
